69d313d15c9c0859d62844a9e9d363d5fd8973c1
CTV <22521653@gm.uit.edu.vn>
2023-12-30 19:07:29 +0700

    initial project

---
+++ coffee-main/Models/DtoEdit/GioHangsDtoEdit.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Coffee.Models.DtoEdit
+{
+    /// <summary>
+    /// Class này hiển thị thêm các thông tin trong giỏ hàng như tên, giá,...
+    /// </summary>
+    [NotMapped]
+    public class GioHangsDtoEdit : ChiTietDonHangsDtoEdit
+    {
+        public new string TenSanPham { get; set; }
+    }
+}
---
+++ coffee-main/Models/DtoEdit/VaiTro.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coffee.Models.DtoEdit
+{
+    public enum VaiTro
+    {
+         NhanVien = 1,
+         QuanLy = 0
+    }
+}
