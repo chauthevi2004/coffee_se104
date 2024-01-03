dc8b323b452299f22d1eae0a2e63d87d6a38c1e2
PhongPhan2k4 <112098006+PhongPhan2k4@users.noreply.github.com>
2024-01-04 00:18:46 +0700

    update RouteConfig.cs

--- Coffee/App_Start/RouteConfig.cs
+++ Coffee/App_Start/RouteConfig.cs
@@ -13,11 +13,64 @@ namespace Coffee
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+              name: "login",
+              url: "login",
+              defaults: new { controller = "Login", action = "FormDangNhap", id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
+              name: "thuc-don",
+              url: "thuc-don",
+              defaults: new { controller = "Home", action = "ThucDon", id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
+             name: "nhan-vien",
+             url: "nhan-vien",
+             defaults: new { controller = "QuanTriNhanVien", action = "Index", id = UrlParameter.Optional }
+         );
+
+            routes.MapRoute(
+             name: "thong-tin-san-pham",
+             url: "thong-tin-san-pham",
+             defaults: new { controller = "SanPham", action = "ThongTin", id = UrlParameter.Optional }
+         );
+
+
+            routes.MapRoute(
+              name: "doanh-thu",
+              url: "doanh-thu",
+              defaults: new { controller = "QuanTriDoanhThu", action = "Index", id = UrlParameter.Optional }
+          );
+
+            routes.MapRoute(
+               name: "danh-sach-don-hang",
+               url: "danh-sach-don-hang",
+               defaults: new { controller = "QuanTriDonHang", action = "Index", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "danh-sach-sp",
+               url: "danh-sach-sp",
+               defaults: new { controller = "QuanTriSP", action = "DanhSachSanPham", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+               name: "cap-nhat-sp",
+               url: "cap-nhat-sp",
+               defaults: new { controller = "QuanTriSP", action = "ThemSanPham", id = UrlParameter.Optional }
+           );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+
+
+
         }
     }
 }
